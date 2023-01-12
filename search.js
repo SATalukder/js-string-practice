432091f951197ee5deac64a2563eb0b51467390b
SATalukder <shajibahmad152@gmail.com>
2023-01-13 01:14:54 +0600

    string search

---
+++ search.js
@@ -0,0 +1,18 @@
+const lyrics = 'Tumi bondhu kala pakhi ami jeno ki. bosonto kale tomai bolte pari ni. kala kala sada sada';
+
+const lyricslowercase = lyrics.toLowerCase();
+// const doesExist = lyrics.includes('pakhi');
+// const doesExist = lyrics.includes('pakhi');
+const searchString = 'Pakhi';
+const doesExist = lyricslowercase.includes(searchString.toLowerCase());
+// console.log(doesExist);
+
+//--------------------------------
+
+//indexOf
+
+console.log(lyrics.indexOf('kala'));
+
+//startsWith
+
+console.log(lyrics.startsWith('Tumi'));
