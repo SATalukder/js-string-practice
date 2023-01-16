8f53b9a2e03e62f500ddc063334b8039b9a9a875
SATalukder <shajibahmad152@gmail.com>
2023-01-16 23:59:05 +0600

    Math.

---
+++ math.js
@@ -0,0 +1,25 @@
+const result = Math.pow(2, 4);
+// console.log(result);
+
+
+const num1 = 25;
+const num2 = 45;
+
+const gap = Math.abs(num1 - num2);
+
+// console.log(gap);
+
+if (gap < 5) {
+    console.log('You guys can be friends');
+}
+else {
+    console.log('You guys stay apart');
+}
+
+
+const number = 2.4568;
+const fullNumber = Math.round(number);
+console.log(fullNumber);
+
+const result1 = Math.ceil(number);
+console.log(result1);
