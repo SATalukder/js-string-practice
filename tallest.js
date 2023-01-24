6cbd0ac1280c1d6c5f4afe362103dcde640e21b8
SATalukder <shajibahmad152@gmail.com>
2023-01-24 19:52:16 +0600

    find min and max

--- tallest.js
+++ tallest.js
@@ -9,6 +9,21 @@ function maxInArray(height) {
     return max;
 }
 
-const height = [167, 190, 120, 165, 137, 200];
+function getMinInArray(numbers) {
+    let min = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (min > numbers[i]) {
+            min = numbers[i];
+        }
+
+    }
+    return min;
+
+}
+
+const height = [167, 190, 120, 165, 137, 300, 65, 200];
 const tallest = maxInArray(height);
+const smollest = getMinInArray(height);
+
 console.log('Tallest persion is : ' + tallest + 'cm');
+console.log('Smallest persion is : ' + smollest + 'cm');
